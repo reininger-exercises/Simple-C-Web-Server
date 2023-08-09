7327924d8cddaed8bcacd24bd5e0c67a45e42a4b
Reid Reininger <reidreininger@outlook.com>
2023-08-08 20:59:53 -0700

    Initialize tcp connection socket

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 // A simple http server.
 
+#include <sys/socket.h>
+
+#define TCP 6
+
 int main(int argc, char[] *args) {
 	// Open TCP socket to listen for connections
+	int connectionFd = socket(PF_INET, SOCK_STREAM, TCP);
 
 	// Once a connection is made:
 
