3430e6aa6f9c071487fff97f653f8b192cffe083
Reid Reininger <reidreininger@outlook.com>
2023-08-08 21:29:35 -0700

    Add print debugging statements

--- main.c
+++ main.c
@@ -1,15 +1,29 @@
 // A simple http server.
 
 #include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char[] *args) {
+#define DEFAULT_PORT 5005
+
+int main(int argc, char *args[]) {
 	// Open TCP socket to listen for connections
 
+	// create socket
 	// for protocol argument, 0 means default (tcp for SOCK_STREAM)
 	int connectionFd = socket(PF_INET, SOCK_STREAM, 0);
 	if (connectionFd == -1) {
+		puts("Unable to create connection socket, program will terminate.");
 		exit(1);
 	}
+
+	printf("connection socket fd: %d\n", connectionFd);
+	// bind socket
+	//int port = 
+	//sockaddr_in address = { .sin_family = AF_INET, .sin_port = DEFAULT_PORT, .
+
+	// listen
+
 
 	// Once a connection is made:
 
